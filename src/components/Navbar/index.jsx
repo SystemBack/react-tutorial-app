730304af00c45e21b201cde1fb3023e322fb944c
Eduardo <systemback@Eduardos-Mac-mini.local>
2023-08-11 10:09:08 -0600

    Create first component(Navbar)

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>Eventos</p>
+            <input placeholder="Busca tú evento favorito" />
+        </div>
+    );
+};
+
+export default Navbar;
